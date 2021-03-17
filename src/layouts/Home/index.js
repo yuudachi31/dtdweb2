59cb71fc173bbec469a6d26283d335a55fb7fdba
Jennifer <jennifer861025@gmail.com>
2021-03-17 21:38:37 +0800

    carousel

--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -2,7 +2,11 @@ import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import styles from './styles.module.scss';
+// import styles from './styles.module.scss';
+import Carousel from 'react-bootstrap/Carousel';
+import banner_1 from '../../assets/images/banners/banner_1.jpg';
+import banner_2 from '../../assets/images/banners/banner_2.jpg';
+import banner_4 from '../../assets/images/banners/banner_4.jpg';
 
 const Home = () => {
   return (
@@ -13,7 +17,19 @@ const Home = () => {
         <meta name="description" content="數位科技設計學系的首頁" />
       </Helmet>
       <Header />
-      <div className={styles.container}>首頁的內容 更動2</div>
+      <div>
+        <Carousel>
+          <Carousel.Item interval={5000}>
+            <img className="d-block w-100" src={banner_1} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <img className="d-block w-100" src={banner_2} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <img className="d-block w-100" src={banner_4} alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
       <Footer />
     </Fragment>
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from '../layouts/Home';
 
 const Routes = () => {
