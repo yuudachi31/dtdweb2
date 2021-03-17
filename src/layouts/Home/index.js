b3b1080863a126999e47b6af999426085b4cb40c
Jennifer <jennifer861025@gmail.com>
2021-03-18 02:15:58 +0800

    index : admission, link

--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -1,12 +1,25 @@
 import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-// import styles from './styles.module.scss';
+import styles from './styles.module.scss';
+import bannerUrl from './banner.json';
+/* react-Bootstrap */
 import Carousel from 'react-bootstrap/Carousel';
-import banner_1 from '../../assets/images/banners/banner_1.jpg';
-import banner_2 from '../../assets/images/banners/banner_2.jpg';
-import banner_4 from '../../assets/images/banners/banner_4.jpg';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+/* images */
+import about from '../../assets/images/index/about.png';
+import works from '../../assets/images/index/works.png';
+import senior from '../../assets/images/index/senior.png';
+import classroom from '../../assets/images/index/classroom.png';
+import Job from '../../assets/images/index/Job.png';
+import StudyGroup from '../../assets/images/index/StudyGroup.png';
+import Fan from '../../assets/images/index/Fan.png';
+import TeachGroup from '../../assets/images/index/TeachGroup.png';
+import WebGroup from '../../assets/images/index/WebGroup.png';
+import Ntue from '../../assets/images/index/Ntue.png';
 
 const Home = () => {
   return (
@@ -20,16 +33,159 @@ const Home = () => {
       <div>
         <Carousel>
           <Carousel.Item interval={5000}>
-            <img className="d-block w-100" src={banner_1} alt="First slide" />
+            <img
+              className="d-block w-100"
+              src={bannerUrl.banner_1}
+              alt="First slide"
+            />
           </Carousel.Item>
           <Carousel.Item interval={5000}>
-            <img className="d-block w-100" src={banner_2} alt="Second slide" />
+            <img
+              className="d-block w-100"
+              src={bannerUrl.banner_2}
+              alt="Second slide"
+            />
           </Carousel.Item>
           <Carousel.Item interval={5000}>
-            <img className="d-block w-100" src={banner_4} alt="Third slide" />
+            <img
+              className="d-block w-100"
+              src={bannerUrl.banner_4}
+              alt="Third slide"
+            />
           </Carousel.Item>
         </Carousel>
       </div>
+
+      <div className={styles.container}>
+        <div className={`${styles.section} ${styles.section_news}`} id="news">
+          <div className={styles.newsContent}>
+            <h1>系務公告 / Announcement</h1>
+          </div>
+          <div className={styles.newsContent}>
+            <h1>活動公告 / Event</h1>
+          </div>
+        </div>
+        <div className={styles.section} id="event">
+          <h1>活動公告 / Event</h1>
+          <hr></hr>
+        </div>
+        <div className={styles.section} id="admissionInformation">
+          <h1>新生快報 / Admission Information </h1>
+          <hr></hr>
+          <Row className={styles.link}>
+            <Col sm={3} xs={12}>
+              <Link>
+                <div className={styles.link_img}>
+                  <img src={about} alt="" className={styles.link_img__size} />
+                </div>
+                <div className={styles.link_p}>
+                  <p>關於數位</p>
+                </div>
+              </Link>
+            </Col>
+            <Col sm={3} xs={12}>
+              <Link>
+                <div className={styles.link_img}>
+                  <img src={works} alt="" className={styles.link_img__size} />
+                </div>
+                <div className={styles.link_p}>
+                  <p>作品展示</p>
+                </div>
+              </Link>
+            </Col>
+            <Col sm={3} xs={12}>
+              <Link>
+                <div className={styles.link_img}>
+                  <img src={senior} alt="" className={styles.link_img__size} />
+                </div>
+                <div className={styles.link_p}>
+                  <p>招生資訊</p>
+                </div>
+              </Link>
+            </Col>
+            <Col sm={3} xs={12}>
+              <Link>
+                <div className={styles.link_img}>
+                  <img
+                    src={classroom}
+                    alt=""
+                    className={styles.link_img__size}
+                  />
+                </div>
+                <div className={styles.link_p}>
+                  <p>教室導覽</p>
+                </div>
+              </Link>
+            </Col>
+          </Row>
+        </div>
+        <div className={`${styles.section} ${styles.section_link}`} id="link">
+          <hr></hr>
+          <Row className={styles.link}>
+            <Col sm={2} xs={6}>
+              <Link>
+                <div className={styles.link_img}>
+                  <img src={Job} alt="" />
+                </div>
+                <div className={styles.link_p}>
+                  <p>就業分析</p>
+                </div>
+              </Link>
+            </Col>
+            <Col sm={2} xs={6}>
+              <Link>
+                <div className={styles.link_img}>
+                  <img src={StudyGroup} alt="" />
+                </div>
+                <div className={styles.link_p}>
+                  <p>讀書會</p>
+                </div>
+              </Link>
+            </Col>
+            <Col sm={2} xs={6}>
+              <Link>
+                <div className={styles.link_img}>
+                  <img src={Fan} alt="" />
+                </div>
+                <div className={styles.link_p}>
+                  <p>粉絲團</p>
+                </div>
+              </Link>
+            </Col>
+            <Col sm={2} xs={6}>
+              <Link>
+                <div className={styles.link_img}>
+                  <img src={TeachGroup} alt="" />
+                </div>
+                <div className={styles.link_p}>
+                  <p>教學團隊</p>
+                </div>
+              </Link>
+            </Col>
+            <Col sm={2} xs={6}>
+              <Link>
+                <div className={styles.link_img}>
+                  <img src={WebGroup} alt="" />
+                </div>
+                <div className={styles.link_p}>
+                  <p>系網團隊</p>
+                </div>
+              </Link>
+            </Col>
+            <Col sm={2} xs={6}>
+              <Link>
+                <div className={styles.link_img}>
+                  <img src={Ntue} alt="" />
+                </div>
+                <div className={styles.link_p}>
+                  <p>國北教大</p>
+                </div>
+              </Link>
+            </Col>
+          </Row>
+        </div>
+      </div>
+
       <Footer />
     </Fragment>
   );
