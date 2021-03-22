0873ca3f213c65a2364b5fcd2cd405b038d69643
Murphy Hsieh <g110834005@grad.ntue.edu.tw>
2021-03-22 10:36:06 +0800

    update documents

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import path from '../utils/path';
+
 import HomePage from '../layouts/Home';
-import TeacherPage from '../layouts/Teacher';
-import TreachdetailPage from '../layouts/TeachDetail';
+import StaffPage from '../layouts/Teacher';
+import StaffDetailPage from '../layouts/TeachDetail';
 import NewsPage from '../layouts/News';
 import HonorsPage from '../layouts/Honors';
 import RulesPage from '../layouts/Rules';
@@ -15,17 +17,17 @@ const Routes = () => {
     <Router>
       <Switch>
         {/* 首頁 */}
-        <Route exact path="/" component={HomePage} />
+        <Route exact path={path.home} component={HomePage} />
         {/* 關於數位 */}
-        <Route exact path="/teacher" component={TeacherPage} />
-        <Route exact path="/teacher/tea" component={TreachdetailPage} />
+        <Route exact path={path.staff} component={StaffPage} />
+        <Route exact path={`${path.staff}/:tea`} component={StaffDetailPage} />
         {/* 最新消息 */}
-        <Route exact path="/news" component={NewsPage} />
-        <Route exact path="/honors" component={HonorsPage} />
+        <Route exact path={path.news} component={NewsPage} />
+        <Route exact path={path.honors} component={HonorsPage} />
         {/* 作品展示 */}
         {/* 下載專區 */}
-        <Route exact path="/rules" component={RulesPage} />
-        <Route exact path="/download" component={DownloadPage} />
+        <Route exact path={path.rules} component={RulesPage} />
+        <Route exact path={path.downloads} component={DownloadPage} />
       </Switch>
     </Router>
   );
