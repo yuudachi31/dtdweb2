b22f8756bd935e6e6ce4f79b10f882000540bd2f
humichen <humi1125maka@gmail.com>
2021-04-01 18:01:20 +0800

    fix:reducer set initalstate

--- src/components/StaffGroup/index.js
+++ src/components/StaffGroup/index.js
@@ -1,65 +1,83 @@
 import React, { useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import * as Scroll from 'react-scroll';
 //路徑
 import path from '../../utils/path';
 //設計
 import styles from './styles.module.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 //取職員資料
 import { getStaff } from '../../store/actions';
 import { StoreContext } from '../../store/reducer';
 
 const StaffGroup = () => {
-  const { state, dispatch } = useContext(StoreContext);
+  const {
+    state: {
+      staff,
+      requestdata: { loading },
+    },
+    dispatch,
+  } = useContext(StoreContext);
+  const geturlid = window.location.href;
   useEffect(() => {
     getStaff(dispatch);
+    if (geturlid.search(/#/i) != -1) {
+      console.log(geturlid.slice(geturlid.search(/#/i) + 1));
+      Scroll.scroller.scrollTo(geturlid.slice(geturlid.search(/#/i) + 1));
+    }
   }, []);
   return (
-    <div className={styles.container}>
-      {state.staff.map((group) => (
-        <div
-          className={styles.container}
-          key={group.groupid}
-          id={'group' + group.groupid}
-        >
-          <div className={styles.staffGroupName}>{group.title}</div>
-          <Row className={styles.staffBar}>
-            {group.list.map((tea) => (
-              <Col
-                key={tea.id}
-                className={styles.staffBar_staffBox}
-                xl={3}
-                lg={6}
-                md={6}
-                sm={12}
-              >
-                <Link
-                  to={
-                    path.staff +
-                    '/' +
-                    tea.englishname +
-                    '?groupid=' +
-                    group.groupid +
-                    '&teacherid=' +
-                    tea.id
-                  }
-                  className={styles.staffBar_staffBox__img}
-                >
-                  <img src={tea.imgurl} />
-                </Link>
-                <div className={styles.staffBar_staffBox__content}>
-                  {tea.title}
-                </div>
-                <div className={styles.staffBar_staffBox__content}>
-                  {tea.teachername}
-                </div>
-              </Col>
-            ))}
-          </Row>
+    <>
+      {loading ? (
+        <div className={styles.container}></div>
+      ) : (
+        <div className={styles.container}>
+          {staff.map((group) => (
+            <div
+              className={styles.container}
+              key={group.groupid}
+              id={'group' + group.groupid}
+            >
+              <div className={styles.staffGroupName}>{group.title}</div>
+              <Row className={styles.staffBar}>
+                {group.list.map((tea) => (
+                  <Col
+                    key={tea.id}
+                    className={styles.staffBar_staffBox}
+                    xl={3}
+                    lg={6}
+                    md={6}
+                    sm={12}
+                  >
+                    <Link
+                      to={
+                        path.staff +
+                        '/' +
+                        tea.englishname +
+                        '?groupid=' +
+                        group.groupid +
+                        '&teacherid=' +
+                        tea.id
+                      }
+                      className={styles.staffBar_staffBox__img}
+                    >
+                      <img src={tea.imgurl} />
+                    </Link>
+                    <div className={styles.staffBar_staffBox__content}>
+                      {tea.title}
+                    </div>
+                    <div className={styles.staffBar_staffBox__content}>
+                      {tea.teachername}
+                    </div>
+                  </Col>
+                ))}
+              </Row>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 };
 
--- src/layouts/StaffDetail/index.js
+++ src/layouts/StaffDetail/index.js
@@ -3,7 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import * as QueryString from 'query-string';
 import * as Scroll from 'react-scroll';
-//componemt&常數
+//componemt&路徑
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import path from '../../utils/path';
@@ -20,12 +20,15 @@ import phoneIcon from '../../assets/images/icons/icon_phone.png';
 import emailIcon from '../../assets/images/icons/icon_email.png';
 
 const StaffDetail = () => {
-  const { state, dispatch } = useContext(StoreContext);
   const location = useLocation();
   const { groupid, teacherid } = QueryString.parse(location.search);
-  console.log(state);
-  console.log(groupid);
-  console.log(teacherid);
+  const {
+    state: {
+      staffDetail,
+      requestdata: { loading },
+    },
+    dispatch,
+  } = useContext(StoreContext);
 
   useEffect(() => {
     console.log('useEffect work');
@@ -33,156 +36,177 @@ const StaffDetail = () => {
     const teacherid2 = parseInt(teacherid);
     getStaffDetail(dispatch, { groupid2, teacherid2 });
     Scroll.scroller.scrollTo('top');
-  }, []);
+  }, [loading]);
 
   return (
-    <Fragment>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{state.staffDetail.teachername}-國立臺北教育大學</title>
-        <meta name="description" content="數位科技設計學系的教室團隊" />
-      </Helmet>
-      <div className={styles.container} id="top">
-        <Header />
-        <div className={styles.staffDetail}>
-          {/* 教師名字 */}
-          <Row className={styles.staffDetail_titleBar}>
-            <Link
-              to={path.staff + '#group' + groupid}
-              className={styles.staffDetail_titleBar__backBtn}
-            >
-              <img src={leftArrow} />
-            </Link>
-            <div className={styles.staffDetail_titleBar__name}>
-              {state.staffDetail.teachername.search(/（/i) == -1
-                ? state.staffDetail.teachername
-                : state.staffDetail.teachername.substring(
-                    0,
-                    state.staffDetail.teachername.search(/（/i),
-                  )}
-            </div>
-          </Row>
-          {/* 教師資訊 */}
-          <Row className={styles.staffDetail_contentBar}>
-            {/* 教師頭像 */}
-            <Col lg={3} md={12} sm={12} xs={12} class="justify-content-center">
-              <div className={styles.staffDetail_contentBar__img}>
-                <img src={'../' + state.staffDetail.imgurl} />
-              </div>
-            </Col>
-            <Col
-              lg={9}
-              md={12}
-              sm={12}
-              className={styles.staffDetail_contentBar__content}
-            >
-              {/* 職稱 */}
-              <div>
-                職稱：
-                {groupid != '3'
-                  ? state.staffDetail.title
-                  : '兼任' + state.staffDetail.title}
-              </div>
-              {/* 電話與email */}
-              <Row className={styles.contactBar}>
-                <Col
-                  xl={3}
-                  lg={4}
-                  md={12}
-                  className={styles.contactBar_contactBox}
+    <>
+      {loading || JSON.stringify(staffDetail) === '{}' ? (
+        <Fragment>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>教學團隊-國立臺北教育大學</title>
+            <meta name="description" content="數位科技設計學系的教室團隊" />
+          </Helmet>
+          <div className={styles.container} id="top">
+            <Header />
+            <div className={styles.staffDetail}></div>
+            <Footer />
+          </div>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>{staffDetail.teachername}-國立臺北教育大學</title>
+            <meta name="description" content="數位科技設計學系的教室團隊" />
+          </Helmet>
+          <div className={styles.container} id="top">
+            <Header />
+            <div className={styles.staffDetail}>
+              {/* 教師名字 */}
+              <Row className={styles.staffDetail_titleBar}>
+                <Link
+                  to={path.staff + '#group' + groupid}
+                  className={styles.staffDetail_titleBar__backBtn}
                 >
-                  <img src={phoneIcon} />
-                  {state.staffDetail.phone != '' ? (
-                    <a
-                      href={
-                        'tel:+886-2-2732-1104' +
-                        (state.staffDetail.phone.search(/、/i) == -1
-                          ? state.staffDetail.phone
-                          : state.staffDetail.phone.substring(
-                              0,
-                              state.staffDetail.phone.search(/、/i),
-                            ))
-                      }
-                    >
-                      校內分機：{state.staffDetail.phone}
-                    </a>
-                  ) : (
-                    <div className={styles.contactBar_contactBox__hint}>
-                      校內分機：無
-                    </div>
-                  )}
+                  <img src={leftArrow} />
+                </Link>
+                <div className={styles.staffDetail_titleBar__name}>
+                  {staffDetail.teachername.search(/（/i) == -1
+                    ? staffDetail.teachername
+                    : staffDetail.teachername.substring(
+                        0,
+                        staffDetail.teachername.search(/（/i),
+                      )}
+                </div>
+              </Row>
+              {/* 教師資訊 */}
+              <Row className={styles.staffDetail_contentBar}>
+                {/* 教師頭像 */}
+                <Col
+                  lg={3}
+                  md={12}
+                  sm={12}
+                  xs={12}
+                  className={styles.staffDetail_contentBar__box}
+                >
+                  <div className={styles.staffDetail_contentBar__img}>
+                    <img src={'../' + staffDetail.imgurl} />
+                  </div>
                 </Col>
-                <Col className={styles.contactBar_contactBox}>
-                  <img src={emailIcon} />
-                  {state.staffDetail.email != '' ? (
-                    <a href={'mailto:' + state.staffDetail.email}>
-                      電子信箱：{state.staffDetail.email}
-                    </a>
-                  ) : (
-                    <div className={styles.contactBar_contactBox__hint}>
-                      電子信箱：無
+                <Col
+                  lg={9}
+                  md={12}
+                  sm={12}
+                  className={styles.staffDetail_contentBar__content}
+                >
+                  {/* 職稱 */}
+                  <div>
+                    職稱：
+                    {groupid != '3'
+                      ? staffDetail.title
+                      : '兼任' + staffDetail.title}
+                  </div>
+                  {/* 電話與email */}
+                  <Row className={styles.contactBar}>
+                    <Col
+                      xl={3}
+                      lg={4}
+                      md={5}
+                      className={styles.contactBar_contactBox}
+                    >
+                      <img src={phoneIcon} />
+                      {staffDetail.phone != '' ? (
+                        <a
+                          href={
+                            'tel:+886-2-2732-1104' +
+                            (staffDetail.phone.search(/、/i) == -1
+                              ? staffDetail.phone
+                              : staffDetail.phone.substring(
+                                  0,
+                                  staffDetail.phone.search(/、/i),
+                                ))
+                          }
+                        >
+                          校內分機：{staffDetail.phone}
+                        </a>
+                      ) : (
+                        <div className={styles.contactBar_contactBox__hint}>
+                          校內分機：無
+                        </div>
+                      )}
+                    </Col>
+                    <Col className={styles.contactBar_contactBox}>
+                      <img src={emailIcon} />
+                      {staffDetail.email != '' ? (
+                        <a href={'mailto:' + staffDetail.email}>
+                          電子信箱：{staffDetail.email}
+                        </a>
+                      ) : (
+                        <div className={styles.contactBar_contactBox__hint}>
+                          電子信箱：無
+                        </div>
+                      )}
+                    </Col>
+                  </Row>
+                  <div className={styles.contactLine}></div>
+                  <div className={styles.contactDetail}>
+                    {/* 辦公室or研究室 */}
+                    <div>
+                      <strong>
+                        {staffDetail.title != '助教' ? '研究室' : '辦公室'}：
+                      </strong>
+                      {staffDetail.room != '' ? staffDetail.room : '無'}
                     </div>
-                  )}
+                    {/* 個人網站（助教為工作職掌）*/}
+                    <div>
+                      <strong>
+                        {staffDetail.title != '助教' ? '個人網站' : '工作職掌'}
+                        ：
+                      </strong>
+                      {staffDetail.title != '助教' ? (
+                        staffDetail.website != '' ? (
+                          <a
+                            href={staffDetail.website}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            {staffDetail.website}
+                          </a>
+                        ) : (
+                          '無'
+                        )
+                      ) : (
+                        staffDetail.skill
+                      )}
+                    </div>
+                    {/* 學歷（助教除外） */}
+                    <div>
+                      <strong>
+                        {staffDetail.title != '助教' ? '學歷：' : ''}
+                      </strong>
+                      {staffDetail.education}
+                    </div>
+                    {/* 專長（助教除外） */}
+                    <div>
+                      <strong>
+                        {staffDetail.title != '助教' ? '專長：' : ''}
+                      </strong>
+                      {staffDetail.title != '助教'
+                        ? staffDetail.skill != ''
+                          ? staffDetail.skill
+                          : '無'
+                        : ''}
+                    </div>
+                  </div>
                 </Col>
               </Row>
-              <div className={styles.contactLine}></div>
-              <div className={styles.contactDetail}>
-                {/* 辦公室or研究室 */}
-                <div>
-                  <strong>
-                    {state.staffDetail.title != '助教' ? '研究室' : '辦公室'}：
-                  </strong>
-                  {state.staffDetail.room != '' ? state.staffDetail.room : '無'}
-                </div>
-                {/* 個人網站（助教為工作職掌）*/}
-                <div>
-                  <strong>
-                    {state.staffDetail.title != '助教'
-                      ? '個人網站'
-                      : '工作職掌'}
-                    ：
-                  </strong>
-                  {state.staffDetail.title != '助教' ? (
-                    state.staffDetail.website != '' ? (
-                      <a
-                        href={state.staffDetail.website}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        {state.staffDetail.website}
-                      </a>
-                    ) : (
-                      '無'
-                    )
-                  ) : (
-                    state.staffDetail.skill
-                  )}
-                </div>
-                {/* 學歷（助教除外） */}
-                <div>
-                  <strong>
-                    {state.staffDetail.title != '助教' ? '學歷：' : ''}
-                  </strong>
-                  {state.staffDetail.education}
-                </div>
-                {/* 專長（助教除外） */}
-                <div>
-                  <strong>
-                    {state.staffDetail.title != '助教' ? '專長：' : ''}
-                  </strong>
-                  {state.staffDetail.title != '助教'
-                    ? state.staffDetail.skill != ''
-                      ? state.staffDetail.skill
-                      : '無'
-                    : ''}
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </div>
-      <Footer />
-    </Fragment>
+            </div>
+          </div>
+          <Footer />
+        </Fragment>
+      )}
+    </>
   );
 };
 
