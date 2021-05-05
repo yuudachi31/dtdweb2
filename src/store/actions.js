880930589d6ba89a71f389b6bb1aac2d1bd910a5
Jennifer <jennifer861025@gmail.com>
2021-05-05 22:30:13 +0800

    refactor: index-banner img change to reducer

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,11 +9,13 @@ import {
   BEGIN_DATA_REQUEST,
   SUCCESS_DATA_REQUEST,
   FAIL_DATA_REQUEST,
+  SET_BANNER,
 } from './actionTypes';
 
 const BASE_URL = '';
 
 import staffjson from '../assets/json/teachers.json';
+import bannerjson from '../assets/json/banner.json';
 
 export const getNews = async (dispatch, options) => {
   const { perPage = 5, page = 1 } = options;
@@ -105,6 +107,21 @@ export const getStaffDetail = async (dispatch, options) => {
     dispatch({
       type: SET_STAFF_DETAIL,
       payload: staffDetail,
+    });
+    dispatch({ type: SUCCESS_DATA_REQUEST });
+  } catch (error) {
+    dispatch({ type: FAIL_DATA_REQUEST, payload: error });
+  }
+};
+
+export const getBanner = async (dispatch) => {
+  dispatch({ type: BEGIN_DATA_REQUEST });
+  try {
+    //從json取資料
+    const banner = bannerjson;
+    dispatch({
+      type: SET_BANNER,
+      payload: banner,
     });
     dispatch({ type: SUCCESS_DATA_REQUEST });
   } catch (error) {
