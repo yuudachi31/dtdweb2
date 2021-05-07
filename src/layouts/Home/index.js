d08e5214edbf20a905af8bc9cbc820247052a14c
Jennifer <jennifer861025@gmail.com>
2021-05-08 02:09:05 +0800

    refactor: add indexNews component

--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -1,18 +1,13 @@
 import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
-import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 /*component*/
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import IndexBanner from '../../components/IndexBanner';
+import IndexNews from '../../components/IndexNews';
 import IndexContent from '../../components/IndexContent';
-
-/*icon*/
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   return (
@@ -24,150 +19,10 @@ const Home = () => {
       </Helmet>
       <Header />
       <IndexBanner />
-
       <div className={styles.container}>
-        <div className={`${styles.section} ${styles.section_news}`} id="news">
-          <div className={styles.newsContent}>
-            <h1>系務公告 / Announcement</h1>
-            <div className={`${styles.news} ${styles.news_top}`}>
-              <Link to="/" className={styles.link_a__textdecnone}>
-                <p className={styles.news_p}>
-                  110學年度碩士班考試入學面試地點公告
-                </p>
-              </Link>
-              <hr className={styles.news_p}></hr>
-              <span className={styles.news_span}>
-                <FontAwesomeIcon icon={faClock} />
-                &ensp;5&ensp;2月,2021
-              </span>
-            </div>
-            <div className={styles.news}>
-              <Link to="/" className={styles.link_a__textdecnone}>
-                <p className={styles.news_p}>
-                  110學年度碩士班考試入學面試地點公告
-                </p>
-              </Link>
-              <hr className={styles.news_p}></hr>
-              <span className={styles.news_span}>
-                <FontAwesomeIcon icon={faClock} />
-                &ensp;5&ensp;2月,2021
-              </span>
-            </div>
-            <div className={styles.news}>
-              <Link to="/" className={styles.link_a__textdecnone}>
-                <p className={styles.news_p}>
-                  110學年度碩士班考試入學面試地點公告
-                </p>
-              </Link>
-              <hr className={styles.news_p}></hr>
-              <span className={styles.news_span}>
-                <FontAwesomeIcon icon={faClock} />
-                &ensp;5&ensp;2月,2021
-              </span>
-            </div>
-            <div className={styles.news}>
-              <Link to="/" className={styles.link_a__textdecnone}>
-                <p className={styles.news_p}>
-                  110學年度碩士班考試入學面試地點公告
-                </p>
-              </Link>
-              <hr className={styles.news_p}></hr>
-              <span className={styles.news_span}>
-                <FontAwesomeIcon icon={faClock} />
-                &ensp;5&ensp;2月,2021
-              </span>
-            </div>
-            <div className={styles.news}>
-              <Link to="/" className={styles.link_a__textdecnone}>
-                <p className={styles.news_p}>
-                  110學年度碩士班考試入學面試地點公告
-                </p>
-              </Link>
-              <hr className={styles.news_p}></hr>
-              <span className={styles.news_span}>
-                <FontAwesomeIcon icon={faClock} />
-                &ensp;5&ensp;2月,2021
-              </span>
-            </div>
-            <button
-              className={`${styles.news_button} ${styles.news_button_mb}`}
-            >
-              <FontAwesomeIcon icon={faAngleRight} />
-              &ensp;&ensp;&ensp;More
-            </button>
-          </div>
-
-          <div className={styles.newsContent}>
-            <h1>師生榮譽榜 / Achievement</h1>
-            <div className={`${styles.news} ${styles.news_top}`}>
-              <Link to="/" className={styles.link_a__textdecnone}>
-                <p className={styles.news_p}>
-                  110學年度碩士班考試入學面試地點公告
-                </p>
-              </Link>
-              <hr className={styles.news_p}></hr>
-              <span className={styles.news_span}>
-                <FontAwesomeIcon icon={faClock} />
-                &ensp;5&ensp;2月,2021
-              </span>
-            </div>
-            <div className={styles.news}>
-              <Link to="/" className={styles.link_a__textdecnone}>
-                <p className={styles.news_p}>
-                  110學年度碩士班考試入學面試地點公告
-                </p>
-              </Link>
-              <hr className={styles.news_p}></hr>
-              <span className={styles.news_span}>
-                <FontAwesomeIcon icon={faClock} />
-                &ensp;5&ensp;2月,2021
-              </span>
-            </div>
-            <div className={styles.news}>
-              <Link to="/" className={styles.link_a__textdecnone}>
-                <p className={styles.news_p}>
-                  110學年度碩士班考試入學面試地點公告
-                </p>
-              </Link>
-              <hr className={styles.news_p}></hr>
-              <span className={styles.news_span}>
-                <FontAwesomeIcon icon={faClock} />
-                &ensp;5&ensp;2月,2021
-              </span>
-            </div>
-            <div className={styles.news}>
-              <Link to="/" className={styles.link_a__textdecnone}>
-                <p className={styles.news_p}>
-                  110學年度碩士班考試入學面試地點公告
-                </p>
-              </Link>
-              <hr className={styles.news_p}></hr>
-              <span className={styles.news_span}>
-                <FontAwesomeIcon icon={faClock} />
-                &ensp;5&ensp;2月,2021
-              </span>
-            </div>
-            <div className={styles.news}>
-              <Link to="/" className={styles.link_a__textdecnone}>
-                <p className={styles.news_p}>
-                  110學年度碩士班考試入學面試地點公告
-                </p>
-              </Link>
-              <hr className={styles.news_p}></hr>
-              <span className={styles.news_span}>
-                <FontAwesomeIcon icon={faClock} />
-                &ensp;5&ensp;2月,2021
-              </span>
-            </div>
-            <button className={styles.news_button}>
-              <FontAwesomeIcon icon={faAngleRight} />
-              &ensp;&ensp;&ensp;More
-            </button>
-          </div>
-        </div>
+        <IndexNews />
         <IndexContent />
       </div>
-
       <Footer />
     </Fragment>
   );
