9a2b9af9f1dd74e0efe1a454aabe6b638275ee06
Jennifer <jennifer861025@gmail.com>
2021-05-25 13:43:01 +0800

    feat: homePage add link

--- src/components/IndexBanner/index.js
+++ src/components/IndexBanner/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from 'react';
 import styles from './styles.module.scss';
 import Carousel from 'react-bootstrap/Carousel';
+import { Link } from 'react-router-dom';
 
 import { getBanner } from '../../store/actions';
 import { StoreContext } from '../../store/reducer';
@@ -27,11 +28,13 @@ const IndexBanner = () => {
           <Carousel indicators={false} controls={false}>
             {banner.map((bannerImg) => (
               <Carousel.Item interval={5000} key={bannerImg.id}>
-                <img
-                  className={`d-block w-100 ${styles.carousel_img__rwdHeight}`}
-                  src={bannerImg.bannerUrl}
-                  alt="First slide"
-                />
+                <Link to="/">
+                  <img
+                    className={`d-block w-100 ${styles.carousel_img__rwdHeight}`}
+                    src={bannerImg.bannerUrl}
+                    alt="First slide"
+                  />
+                </Link>
               </Carousel.Item>
             ))}
           </Carousel>
