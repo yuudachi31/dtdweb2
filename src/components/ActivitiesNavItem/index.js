1635b211e9b2f387ddccddeca8ec5473f252ecba
Jennifer <jennifer861025@gmail.com>
2021-06-07 16:39:49 +0800

    feat: add ActivitiesContent camp, visits, exhibitions

--- src/components/ActivitiesNavItem/index.js
+++ src/components/ActivitiesNavItem/index.js
@@ -9,6 +9,9 @@ import { setPageContent, setActiveNavItem } from '../../uiStore/actions';
 /* json檔 */
 import DTDActivities from '../../assets/json/DTDActivities.json';
 import DTDGroup from '../../assets/json/DTDGroup.json';
+import camp from '../../assets/json/camp.json';
+import visits from '../../assets/json/visits.json';
+import exhibition from '../../assets/json/exhibition.json';
 
 const ActivitiesNavItem = (prop) => {
   const { children, pathTo, className, activeClassName } = prop;
@@ -20,6 +23,12 @@ const ActivitiesNavItem = (prop) => {
         return DTDActivities;
       case `${path.activities}/DTDGroup`:
         return DTDGroup;
+      case `${path.activities}/camp`:
+        return camp;
+      case `${path.activities}/visits`:
+        return visits;
+      case `${path.activities}/exhibition`:
+        return exhibition;
       default:
         return DTDActivities;
     }
