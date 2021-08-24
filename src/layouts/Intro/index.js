0a1facd3fb43377f9e06f87b4e1e505b29d02547
Uma <sarapan19980221@gmail.com>
2021-08-24 17:58:32 +0800

    style: introPage change youtube link

--- src/layouts/Intro/index.js
+++ src/layouts/Intro/index.js
@@ -52,7 +52,7 @@ const Intro = () => {
                 className={styles.aboutBlock_video}
                 width="560"
                 height="315"
-                src="https://www.youtube.com/embed/Zl3CNCsZB9c?wmode=transparent"
+                src="https://www.youtube.com/embed/5HyM0qZEhRw?wmode=transparent"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
