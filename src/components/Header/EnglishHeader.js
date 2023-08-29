55704737d6b1f33655b04ac81fa7d2e8c3630c86
yuudachi31 <yuudachi31@gmail.com>
2023-08-29 17:16:18 +0800

    feat:tony's part 50%

---
+++ src/components/Header/EnglishHeader.js
@@ -0,0 +1,57 @@
+import React, { useContext, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+// 設計
+import styles from './styles.module.scss';
+// 路徑
+import path from '../../utils/path';
+// 圖片匯入
+import logo from '../../assets/images/header/DTD.png';
+// icon匯入
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faBars } from '@fortawesome/free-solid-svg-icons';
+// uiStore
+import { clickHamburgerLink } from '../../uiStore/actions';
+import { UIStoreContext } from '../../uiStore/reducer';
+
+const Header = () => {
+  const { uiState, uiDispatch } = useContext(UIStoreContext);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const listener = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        clickHamburgerLink(uiDispatch);
+        console.log(uiState);
+      }
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref]);
+
+  return (
+    <div className={styles.header}>
+      <Link to={path.home} onClick={() => clickHamburgerLink(uiDispatch)}>
+        <img className={styles.header_logo__width} src={logo}></img>
+      </Link>
+      <div className={styles.header_nav} ref={ref}>
+        {/* navbar */}
+
+        <button className={styles.nav_dropdownMenu_english}>
+          <Link to={path.home}>
+            <div className={styles.dropdownMenu_title_english}>中文版</div>
+          </Link>
+        </button>
+
+        {/* hamburgerMenu */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/layouts/Intro copy/index.js
@@ -0,0 +1,205 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import * as Scroll from 'react-scroll';
+import Carousel from 'react-bootstrap/Carousel';
+// 設計
+import styles from './styles.module.scss';
+// components
+import Header from '../Header';
+import Banner from '../Banner';
+import PageTitle from '../PageTitle';
+import Footer from '../Footer';
+// icon匯入
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+// 資料匯入
+import introjson from '../../assets/json/intro.json';
+
+const Intro = () => {
+  let history = useHistory();
+  const [introDetail, setIntroDetail] = useState({});
+
+  useEffect(() => {
+    setIntroDetail(introjson);
+    if (
+      history.location.state !== undefined &&
+      history.location.state.prevPath === '/' &&
+      introDetail.classrooms
+    ) {
+      Scroll.scroller.scrollTo('classroom');
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [introDetail, history.location.state]);
+
+  return (
+    <Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>本系簡介-國立臺北教育大學</title>
+        <meta name="description" content="數位科技設計學系的本系簡介" />
+      </Helmet>
+      <Header />
+      <Banner bannerNumber={4} />
+      <>
+        {introDetail.about_content &&
+        introDetail.concepts &&
+        introDetail.classrooms ? (
+          <div className={styles.container}>
+            <PageTitle title="本系簡介" />
+            <div
+              className={`${styles.intro_block__marginTop} ${styles.intro_aboutBlock}`}
+            >
+              <iframe
+                className={styles.aboutBlock_video}
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/5HyM0qZEhRw?wmode=transparent"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+              <div className={styles.aboutBlock_content}>
+                <div>{introDetail.about_content}</div>
+                <a
+                  href={introDetail.experience_share_URL}
+                  className={styles.aboutBlock_experienceShare}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className={styles.experienceShare_icon}>
+                    <FontAwesomeIcon icon={faExternalLinkAlt} />
+                  </div>
+                  <div className={styles.experienceShare_content}>
+                    {introDetail.experience_share}
+                  </div>
+                </a>
+              </div>
+            </div>
+            <PageTitle title="教育理念" />
+            <div
+              className={`${styles.intro_block__marginTop} ${styles.intro_conceptBlock}`}
+            >
+              {introDetail.concepts.map((concept) => (
+                <div
+                  className={styles.conceptBlock_conceptBox}
+                  key={concept.concept_title}
+                >
+                  <img src={concept.concept_img}></img>
+                  <div className={styles.conceptBox_title}>
+                    {concept.concept_title}
+                  </div>
+                  <div className={styles.conceptBox_content}>
+                    {concept.concept_content}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div id="classroom">
+              <PageTitle title="教室導覽" />
+              <div className={styles.intro_block__marginTop}>
+                {/* 教室資訊輪播 */}
+                <div className={styles.intro_classroomCarousel}>
+                  <Carousel
+                    indicators={false}
+                    className={styles.classroomCarousel_chevron}
+                  >
+                    {introDetail.classrooms.map((classroom) => (
+                      <Carousel.Item
+                        interval={5000}
+                        key={classroom.classroom_id}
+                      >
+                        <img
+                          className={styles.classroomCarousel_img}
+                          src={classroom.classroom_img}
+                        />
+                        <div
+                          className={
+                            styles.classroomCarousel_classroomCarouselInfo
+                          }
+                        >
+                          <div
+                            className={
+                              styles.classroomCarouselInfo_classroomCarouselName
+                            }
+                          >
+                            {classroom.classroom_name}
+                          </div>
+                          {classroom.classroom_info.map((infoDetail) => (
+                            <div
+                              className={
+                                styles.classroomCarouselInfo_classroomCarouselRow
+                              }
+                              key={infoDetail.info_content}
+                            >
+                              <div
+                                className={`${styles.classroomCarouselRow_title} ${styles.intro_classroomInfo__fontSize}`}
+                              >
+                                {infoDetail.info_title}
+                              </div>
+                              <div
+                                className={`${styles.classroomCarouselRow_content} ${styles.intro_classroomInfo__fontSize}`}
+                              >
+                                {infoDetail.info_content}
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </Carousel.Item>
+                    ))}
+                  </Carousel>
+                </div>
+                {/* 教室資訊區塊 */}
+                <div className={styles.intro_classroomBlock}>
+                  {introDetail.classrooms.map((classroom) => (
+                    <div key={classroom.classroom_id}>
+                      <img
+                        src={classroom.classroom_img}
+                        className={styles.classroomBlock_img}
+                      />
+                      <div className={styles.classroomBlock_classroomBlockInfo}>
+                        <div
+                          className={
+                            styles.classroomBlockInfo_classroomBlockName
+                          }
+                        >
+                          {classroom.classroom_name}
+                        </div>
+                        {classroom.classroom_info.map((infoDetail) => (
+                          <div
+                            className={
+                              styles.classroomBlockInfo_classroomBlockRow
+                            }
+                            key={infoDetail.info_content}
+                          >
+                            <div
+                              className={`${styles.classroomBlockRow_title} ${styles.intro_classroomInfo__fontSize}`}
+                            >
+                              {infoDetail.info_title}
+                            </div>
+                            <div
+                              className={`${styles.classroomBlockRow_content} ${styles.intro_classroomInfo__fontSize}`}
+                            >
+                              {infoDetail.info_content}
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className={styles.container}></div>
+        )}
+      </>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Intro;
